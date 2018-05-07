b9bd5d9f8d1ca6a07e58187539706c8c3dab37ac
ryanbillingsley <ryanbbillingsley@gmail.com>
2018-05-07 13:05:59 -0400

    Vim, zsh, and tmux config

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -10,6 +10,8 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
+alias gap='git add -p'
+alias gcm='git commit -m'
 alias gca='git commit -a'
 alias gcam='git commit -am'
 alias gco='git checkout'
