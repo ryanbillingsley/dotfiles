2414db4bc74ea01d58b59cd614e054d54351e0e1
ryanbillingsley <ryanbbillingsley@gmail.com>
2018-10-24 09:46:36 -0400

    added some aliases, updated vim plugin manager

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,7 @@
 alias compose='docker-compose'
 alias py='python3'
 alias pip='pip3'
+
+alias shell_db='psql -h publc-read-slave.prd.clustertruck.net -Uclustertruck clustertruck_prd'
+alias shell_redshift_prd='psql -h clustertruck-prd.cqrwvjbsnbgc.us-east-2.redshift.amazonaws.com -p 5439 -Udeveloper clustertruck'
+alias shell_redshift_staging='psql -h clustertruck-staging.cbux6opjiyov.us-east-2.redshift.amazonaws.com -p5439 -Udeveloper clustertruck'
